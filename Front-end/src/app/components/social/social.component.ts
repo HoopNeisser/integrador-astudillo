dc239f944dcb701ee8450252bd0f82c6fcd71b3d
Agustin Ezequiel Astudillo <agustin.astudillo97@outlook.es>
2023-05-04 15:52:34 -0300

    Log in primer etapa

--- Front-end/src/app/components/social/social.component.ts
+++ Front-end/src/app/components/social/social.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-social',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./social.component.css']
 })
 export class SocialComponent {
+  //Se crea el constructor con router para crear el metodo login
+  constructor(private router:Router) {}
 
+  login(){
+    this.router.navigate(['/login'])
+  }
 }
